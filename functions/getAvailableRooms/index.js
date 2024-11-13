df3cb4700aeb50cc91bbc02e07930f0b0569107f
Linus Ilbratt <linusilbratt@gmail.com>
2024-11-13 11:13:57 +0100

    changed getAvailableRooms so it returns an array of rooms

--- functions/getAvailableRooms/index.js
+++ functions/getAvailableRooms/index.js
@@ -1,21 +1,17 @@
 import { DynamoDBClient, QueryCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
-import { sendResponse } from '../../responses/index.js';
 
 const dynamoDbClient = new DynamoDBClient({ region: "eu-north-1" });
 
-export const checkAvailableRooms = async (event) => {
+export const checkAvailableRooms = async (roomType) => { 
     try {
-        // GET roomType from path parameters (e.g., /available-rooms/single)
-        const roomType = event.pathParameters?.roomType;
-
         let command;
-        
+
         if (roomType) {
-            // Query if a specific room type is provided
+            
             const params = {
                 TableName: "HotelRooms",
-                IndexName: "RoomTypeAvailableIndex",
+                IndexName: "RoomTypeAvailableIndex", 
                 KeyConditionExpression: "RoomType = :roomType AND available = :available",
                 ExpressionAttributeValues: {
                     ":roomType": { S: roomType },
@@ -24,7 +20,7 @@ export const checkAvailableRooms = async (event) => {
             };
             command = new QueryCommand(params);
         } else {
-            // Scan to get all available rooms if no specific room type is provided
+
             const params = {
                 TableName: "HotelRooms",
                 FilterExpression: "available = :available",
@@ -36,11 +32,11 @@ export const checkAvailableRooms = async (event) => {
         }
 
         const result = await dynamoDbClient.send(command);
-        const rooms = result.Items.map((item) => unmarshall(item));
 
-        return sendResponse(200, { rooms });
+        return result.Items ? result.Items.map((item) => unmarshall(item)) : [];
     } catch (error) {
         console.error("Error fetching available rooms:", error);
-        return sendResponse(500, { success: false, message: "Internal Server Error" });
+
+        throw error;
     }
 };
